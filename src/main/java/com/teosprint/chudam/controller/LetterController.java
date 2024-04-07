19673b4812865a15be155afb14a6df63082e6bfd
songhyeonpk <ehftozl1997@gmail.com>
2024-04-07 22:48:36 +0900

    feature/get-letter : 편지 조회 요청 구현

--- src/main/java/com/teosprint/chudam/controller/LetterController.java
+++ src/main/java/com/teosprint/chudam/controller/LetterController.java
@@ -1,7 +1,12 @@
 package com.teosprint.chudam.controller;
 
+import com.teosprint.chudam.dto.LetterInfoResponseDto;
 import com.teosprint.chudam.service.LetterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LetterController {
 
     private final LetterService letterService;
+
+    // 편지 조회
+    @GetMapping("/api/letter/{letterId}")
+    public ResponseEntity<LetterInfoResponseDto> getLetter(@PathVariable Long letterId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(letterService.getLetter(letterId));
+    }
 }
