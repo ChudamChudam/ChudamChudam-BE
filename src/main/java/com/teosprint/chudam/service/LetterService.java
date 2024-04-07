b44cd09c9d2d74d39f85437bcd7fc4a86115fd7a
songhyeonpk <ehftozl1997@gmail.com>
2024-04-07 22:48:17 +0900

    feature/get-letter : 편지 조회 로직 구현

--- src/main/java/com/teosprint/chudam/service/LetterService.java
+++ src/main/java/com/teosprint/chudam/service/LetterService.java
@@ -1,5 +1,10 @@
 package com.teosprint.chudam.service;
 
 
+import com.teosprint.chudam.dto.LetterInfoResponseDto;
+
 public interface LetterService {
+
+    // 편지 조회
+    LetterInfoResponseDto getLetter(Long letterId);
 }
