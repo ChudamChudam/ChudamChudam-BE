52a9d92e10ed8eb5cdc8f488826cd76f75f4bbed
nayeon <kknyapple@gmail.com>
2024-04-08 09:11:14 +0900

    feature/letter-create : 편지 생성 요청 구현

--- src/main/java/com/teosprint/chudam/controller/LetterController.java
+++ src/main/java/com/teosprint/chudam/controller/LetterController.java
@@ -1,13 +1,13 @@
 package com.teosprint.chudam.controller;
 
+import com.teosprint.chudam.dto.CreateLetterRequestDto;
+import com.teosprint.chudam.dto.CreateLetterResponseDto;
 import com.teosprint.chudam.dto.LetterInfoResponseDto;
 import com.teosprint.chudam.service.LetterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,4 +21,12 @@ public class LetterController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(letterService.getLetter(letterId));
     }
+
+    @PostMapping("/api/letter")
+    public ResponseEntity<CreateLetterResponseDto> uploadLetter(@ModelAttribute CreateLetterRequestDto createLetterRequest){
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(letterService.createLetter(createLetterRequest));
+    }
+
 }
