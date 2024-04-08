177a008ce9d6ce71ed6231e92365c0c82cc875c0
songhyeonpk <ehftozl1997@gmail.com>
2024-04-08 20:08:37 +0900

    fix : 편지 생성 요청 dto 필드 타입 수정

--- src/main/java/com/teosprint/chudam/dto/CreateLetterRequestDto.java
+++ src/main/java/com/teosprint/chudam/dto/CreateLetterRequestDto.java
@@ -10,5 +10,5 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 public class CreateLetterRequestDto {
 
-    private MultipartFile letterImage;
+    private String letterImage;
 }
