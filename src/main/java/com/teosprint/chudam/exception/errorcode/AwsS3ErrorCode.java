9e1ac379d0a533b71c76bc8d2597ff74a8c9965b
nayeon <kknyapple@gmail.com>
2024-04-08 16:09:56 +0900

    featuer/create-letter : AWS S3 설정

---
+++ src/main/java/com/teosprint/chudam/exception/errorcode/AwsS3ErrorCode.java
@@ -0,0 +1,25 @@
+package com.teosprint.chudam.exception.errorcode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AwsS3ErrorCode implements ErrorCode {
+    INVALID_EXTENSION("[확장자오류] : jpg, jpeg, svg, png 파일만 등록 가능합니다", HttpStatus.BAD_REQUEST),
+    EMPTY_UPLOAD_FILE("[빈파일오류] : 업로드 파일이 없습니다", HttpStatus.BAD_REQUEST);
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    @Override
+    public HttpStatus getStatus() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+}
