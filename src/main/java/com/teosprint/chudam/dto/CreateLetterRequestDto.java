f5626d760bd82e20c736868788217d21b65a9e68
nayeon <kknyapple@gmail.com>
2024-04-08 16:10:26 +0900

    feature/create-letter : 편지 생성 DTO 생성

---
+++ src/main/java/com/teosprint/chudam/dto/CreateLetterRequestDto.java
@@ -0,0 +1,14 @@
+package com.teosprint.chudam.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class CreateLetterRequestDto {
+
+    private MultipartFile letterImage;
+}
