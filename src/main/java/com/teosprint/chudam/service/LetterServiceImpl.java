3b134be8c6d9d750a7ed9e2bad397924e09816b0
nayeon <kknyapple@gmail.com>
2024-04-08 15:22:00 +0900

    revert/commit : revert 54b7527d3b3248c258f926a4d8420307eba97dce

--- src/main/java/com/teosprint/chudam/service/LetterServiceImpl.java
+++ src/main/java/com/teosprint/chudam/service/LetterServiceImpl.java
@@ -1,21 +1,12 @@
 package com.teosprint.chudam.service;
 
-import com.teosprint.chudam.config.s3.AwsS3FileResponse;
-import com.teosprint.chudam.config.s3.AwsS3Service;
 import com.teosprint.chudam.domain.Letter;
-import com.teosprint.chudam.dto.CreateLetterRequestDto;
-import com.teosprint.chudam.dto.CreateLetterResponseDto;
 import com.teosprint.chudam.dto.LetterInfoResponseDto;
 import com.teosprint.chudam.exception.custom.LetterCustomException;
 import com.teosprint.chudam.repository.LetterRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
-
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LetterServiceImpl implements LetterService {
@@ -30,28 +21,4 @@ public class LetterServiceImpl implements LetterService {
         LetterInfoResponseDto letterInfoResponse = LetterInfoResponseDto.from(letter.getLetterImageUrl());
         return letterInfoResponse;
     }
-
-    private final AwsS3Service awsS3Service;
-
-    @Override
-    public CreateLetterResponseDto createLetter(CreateLetterRequestDto createLetterRequest) {
-        try{
-            MultipartFile multipartFile = createLetterRequest.getLetterImage();
-            if(multipartFile != null){
-                AwsS3FileResponse response = awsS3Service.uploadFile(multipartFile);
-                Letter letter = letterRepository.save(Letter.createLetter(response.getUrl(), LocalDateTime.now()));
-                log.info("저장된 링크 : [{}]", letter.getLetterImageUrl());
-                log.info("저장된 파일 : [{}]", multipartFile);
-
-                CreateLetterResponseDto createLetterResponseDto = CreateLetterResponseDto.from(letter.getId());
-
-                return createLetterResponseDto;
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-            // 예외처리
-        }
-        return null;
-    }
-
 }
